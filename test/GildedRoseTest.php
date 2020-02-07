b00041722a2d16de9664a36914388f7489b8bc0b
Kristian Muñoz <kristian.munoz@codurance.com>
2020-02-07 16:39:36 +0100

    refactoring tests for readibility and maintainability

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -2,10 +2,69 @@
 
 namespace App;
 
-class GildedRoseTest extends \PHPUnit\Framework\TestCase {
-    
+use Throwable;
 
-    public function testName()
+class GildedRoseTest extends \PHPUnit\Framework\TestCase {
+    const REGULAR_ITEM_NAME = "Regular Item";
+    const IRRELEVANT_SELL_BY_DATE = 10;
+    const DEFAULT_INITIAL_QUALITY = 10;
+    const DEFAULT_QUALITY_AFTER_ONE_DAY = 9;
+    const MAXIMUM_ITEM_QUALITY = 50;
+
+    /**
+     * @test
+     */
+    public function itShouldDecreaseQualityAtTheEndOfTheDayForRegularItems()
+    {
+        $item = $this->generateRegularItem();
+        $gildedRose = new GildedRose([$item]);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(self::DEFAULT_QUALITY_AFTER_ONE_DAY, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldNeverHaveANegativeQualityRegularItems() {
+        $item = $this->generateRegularItemWithZeroQuality();
+        $gildedRose = new GildedRose([$item]);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(0, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldIncreaseInQualityIfAgedBrieItem() {
+        $item = $this->generateBrieItemWithZeroQuality();
+        $gildedRose = new GildedRose([$item]);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(1, $item->quality);
+    }
+
+
+    /**
+     * @test
+     */
+    public function itShouldNeverIncreaseInQualityOverFiftyIfAgedBrieItem() {
+        $item = $this->generateBrieItemWithFiftyQuality();
+        $gildedRose = new GildedRose([$item]);
+
+        $gildedRose->updateQuality();
+
+        $this->assertEquals(self::MAXIMUM_ITEM_QUALITY, $item->quality);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldAgeAllItemsAccordingToFullRulesSet()
     {
         $outputFileName = __DIR__ . '/result.txt';
         $output = fopen($outputFileName, 'wb');
@@ -67,6 +126,48 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase {
         return $result;
     }
 
+    /**
+     * @return Item
+     */
+    private function generateRegularItem(): Item
+    {
+        $item = new Item(self::REGULAR_ITEM_NAME,
+            self::IRRELEVANT_SELL_BY_DATE,
+            self::DEFAULT_INITIAL_QUALITY);
+        return $item;
+    }
+
+    /**
+     * @return Item
+     */
+    private function generateRegularItemWithZeroQuality(): Item
+    {
+        $item = $this->generateRegularItem();
+        $item->quality = 0;
+        return $item;
+    }
+
+
+
+    /**
+     * @return Item
+     */
+    private function generateBrieItemWithZeroQuality(): Item
+    {
+        $item = $this->generateRegularItemWithZeroQuality();
+        $item->name = GildedRose::ITEM_NAME_AGED_BRIE;
+        return $item;
+    }
+
+    /**
+     * @return Item
+     */
+    private function generateBrieItemWithFiftyQuality(): Item
+    {
+        $brieItem = $this->generateBrieItemWithZeroQuality();
+        $brieItem->quality = 50;
+        return $brieItem;
+    }
 
 
 }
