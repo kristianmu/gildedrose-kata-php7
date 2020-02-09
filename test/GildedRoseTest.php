acedd894888f7e81cf961d7c94fea92ba3ac9da6
Kristian Muñoz <kristian.munoz@codurance.com>
2020-02-09 05:30:50 +0100

    Make the acceptance test directly work with the fixtures script

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -181,37 +181,7 @@ class GildedRoseTest extends TestCase
      */
     public function itShouldAgeAllItemsAccordingToFullRulesSet()
     {
-        $output = "OMGHAI!\n";
-
-        $items = array(
-            new GenericRuledItem(new Item('+5 Dexterity Vest', 10, 20)),
-            new AgedBrieRuledItem(new Item('Aged Brie', 2, 0)),
-            new GenericRuledItem(new Item('Elixir of the Mongoose', 5, 7)),
-            new SulfurasRuledItem(new Item('Sulfuras, Hand of Ragnaros', 0, 80)),
-            new SulfurasRuledItem(new Item('Sulfuras, Hand of Ragnaros', -1, 80)),
-            new BackstagePassRuledItem(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)),
-            new BackstagePassRuledItem(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49)),
-            new BackstagePassRuledItem(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49)),
-            new ConjuredRuledItem(new Item('Conjured Mana Cake', 3, 6))
-        );
-
-        $app = new GildedRose($items);
-
-        $days = 2;
-        for ($i = 0; $i < $days; $i++) {
-            $output .= "-------- day $i --------\n";
-            $output .= "name, sellIn, quality\n";
-
-            foreach ($items as $item) {
-                $output .= $item . PHP_EOL;
-            }
-
-            $output .= PHP_EOL;
-
-            $app->updateQuality();
-        }
-
-        $this->assertEquals($output, file_get_contents(__DIR__ . "/goldenmaster.txt"));
+        $this->assertEquals(shell_exec("php ".__DIR__."/../fixtures/texttest_fixture.php"), file_get_contents(__DIR__ . "/goldenmaster.txt"));
     }
 
     private function generateRegularItem($name = self::REGULAR_ITEM_NAME, $quality = self::DEFAULT_INITIAL_QUALITY, $sell_by = self::IRRELEVANT_SELL_IN_DAYS): Item
