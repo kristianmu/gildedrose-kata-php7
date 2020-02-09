328c85a1ef3f3cb054dd5c9de0a87001e4168cc6
Kristian Muñoz <kristian.munoz@codurance.com>
2020-02-09 05:06:33 +0100

    Change the initial fixtures to use the new objects

--- fixtures/texttest_fixture.php
+++ fixtures/texttest_fixture.php
@@ -2,8 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\AgedBrieRuledItem;
+use App\BackstagePassRuledItem;
+use App\ConjuredRuledItem;
+use App\GenericRuledItem;
 use App\GildedRose;
 use App\Item;
+use App\SulfurasRuledItem;
 
 $outputFileName = __DIR__ . '/goldenmaster.txt';
 $output = fopen( $outputFileName, 'wb');
@@ -11,16 +16,16 @@ $output = fopen( $outputFileName, 'wb');
 fputs($output, "OMGHAI!\n");
 
 $items = array(
-    new Item('+5 Dexterity Vest', 10, 20),
-    new Item('Aged Brie', 2, 0),
-    new Item('Elixir of the Mongoose', 5, 7),
-    new Item('Sulfuras, Hand of Ragnaros', 0, 80),
-    new Item('Sulfuras, Hand of Ragnaros', -1, 80),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
-    new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+    new GenericRuledItem(new Item('+5 Dexterity Vest', 10, 20)),
+    new AgedBrieRuledItem(new Item('Aged Brie', 2, 0)),
+    new GenericRuledItem(new Item('Elixir of the Mongoose', 5, 7)),
+    new SulfurasRuledItem(new Item('Sulfuras, Hand of Ragnaros', 0, 80)),
+    new SulfurasRuledItem(new Item('Sulfuras, Hand of Ragnaros', -1, 80)),
+    new BackstagePassRuledItem(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)),
+    new BackstagePassRuledItem(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49)),
+    new BackstagePassRuledItem(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49)),
     // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6)
+    new ConjuredRuledItem(new Item('Conjured Mana Cake', 3, 6))
 );
 
 $app = new GildedRose($items);
--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,10 +4,10 @@ namespace App;
 
 final class GildedRose
 {
-
     const ITEM_NAME_AGED_BRIE = 'Aged Brie';
     const ITEM_NAME_BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = 'Backstage passes to a TAFKAL80ETC concert';
     const ITEM_NAME_SULFURAS_HAND_OF_RAGNAROS = 'Sulfuras, Hand of Ragnaros';
+
     private $items = [];
 
     public function __construct($items)
@@ -18,144 +18,8 @@ final class GildedRose
     public function updateQuality()
     {
         foreach ($this->items as $item) {
-            if ($item instanceof GenericRuledItem) {
-                $item->updateItemQuality();
-                $item->updateItemSellByDate();
-            } else {
-                if ($this->isAgedBrie($item)) {
-                    $this->updateAgedBrieQuality($item);
-                    $this->decreaseSellByDate($item);
-                } else if ($this->isBackStagePass($item)) {
-                    $this->updateBackstagePassQuality($item);
-                    $this->decreaseSellByDate($item);
-                } else if ($this->isSulfura($item)) {
-                    // Sulfuras does nothing
-                } else {
-                    $this->updateRegularItemQuality($item);
-                    $this->decreaseSellByDate($item);
-                }
-            }
-        }
-    }
-
-    /**
-     * @param $item
-     * @return bool
-     */
-    private function isAgedBrie($item): bool
-    {
-        return ($item->name == self::ITEM_NAME_AGED_BRIE);
-    }
-
-    /**
-     * @param $item
-     * @return bool
-     */
-    private function isBackStagePass($item): bool
-    {
-        return ($item->name == self::ITEM_NAME_BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT);
-    }
-
-    /**
-     * @param $item
-     * @return bool
-     */
-    private function isSulfura($item): bool
-    {
-        return ($item->name == self::ITEM_NAME_SULFURAS_HAND_OF_RAGNAROS);
-    }
-
-    /**
-     * @param $item
-     * @return bool
-     */
-    private function hasQuality($item): bool
-    {
-        return $item->quality > 0;
-    }
-
-    /**
-     * @param $item
-     */
-    private function decreaseQuality($item): void
-    {
-        if ($this->hasQuality($item)) {
-            $item->quality = $item->quality - 1;
-        }
-    }
-
-    /**
-     * @param $item
-     */
-    private function increaseQuality($item): void
-    {
-        if (!$this->hasMaximumQuality($item)) {
-            $item->quality = $item->quality + 1;
-        }
-    }
-
-    /**
-     * @param $item
-     * @return bool
-     */
-    private function hasMaximumQuality($item): bool
-    {
-        return ($item->quality >= 50);
-    }
-
-    /**
-     * @param $item
-     */
-    private function decreaseSellByDate($item): void
-    {
-        $item->sell_in = $item->sell_in - 1;
-    }
-
-    /**
-     * @param $item
-     * @return bool
-     */
-    private function isExpired($item): bool
-    {
-        return $item->sell_in < 0;
-    }
-
-    /**
-     * @param $item
-     */
-    private function updateBackstagePassQuality($item): void
-    {
-        $this->increaseQuality($item);
-        if ($item->sell_in < 11) {
-            $this->increaseQuality($item);
-        }
-        if ($item->sell_in < 6) {
-            $this->increaseQuality($item);
-        }
-        if ($this->isExpired($item)) {
-            $item->quality = $item->quality - $item->quality;
-        }
-    }
-
-    /**
-     * @param $item
-     */
-    private function updateAgedBrieQuality($item): void
-    {
-        $this->increaseQuality($item);
-        if ($this->isExpired($item)) {
-            $this->increaseQuality($item);
-        }
-    }
-
-    /**
-     * @param $item
-     */
-    private function updateRegularItemQuality($item): void
-    {
-        $this->decreaseQuality($item);
-        if ($this->isExpired($item)) {
-            $this->decreaseQuality($item);
+            $item->updateItemQuality();
+            $item->updateItemSellByDate();
         }
     }
 }
